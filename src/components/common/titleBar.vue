89ec53ab458202b05fbfb0dcca43da31b3f0dfbc
Hrealm <952172047@qq.com>
2019-06-22 14:34:30 +0800

    更新商铺模块

--- src/components/common/titleBar.vue
+++ src/components/common/titleBar.vue
@@ -1,16 +1,16 @@
 <template>
-	<div>
-		<header class="hd">
+	<div class="hdWrap">
+		<header class="hd clearFix">
 			<a href="javascript:;" class="fl" @click="goBack">
 				<i class="iconfont icon-fanhui"></i>
 			</a>
-            <a href="javascript:;" class="fr">
+			<a href="javascript:;" class="fr">
 				<i class="iconfont icon-gengduo"></i>
 			</a>
-            <div class="search">
-                <i class="iconfont icon-sousuo"></i>
-			    <span>{{title}}</span>
-            </div>
+			<div class="search">
+				<i class="iconfont icon-sousuo"></i>
+				<span>{{title}}</span>
+			</div>
 		</header>
 	</div>
 </template>
@@ -21,55 +21,57 @@ export default {
 		return {}
 	},
 	props: ['title'], //接受父级参数
-    components: {},
-    methods:{
-        goBack(){
-            this.$router.go(-1)
-        }
-    }
+	components: {},
+	methods: {
+		goBack() {
+			this.$router.go(-1)
+		}
+	}
 }
 </script>
 
 <style scoped lang="less">
 @rem: 750/10rem;
 
-.hd{
-    position: fixed;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
+.hdWrap {
     height: 95 /@rem;
-    line-height: 95 /@rem;
-    padding: 0 75/4 /@rem;
-    border-bottom: 0.5px solid #F5F5F9;
-    background-color: white;
-    z-index: 996;
-    font-size: 32/@rem;
-    i{
-        font-size: 32/@rem;
-    }
-    a{
-        padding: 0 10 /@rem;
-        color: #2c3e50;
-    }
-    .search{
-        position: absolute;
-        top: 50%;
-        left: 0;
-        right: 0;
-        margin: 0 auto;
-        width: 75%;
-        height: 75%;
-        background-color: #F5F5F9;
-        transform: translateY(-50%);
-        line-height: 71 /@rem;
-        border-radius: 8px;
-        span{
-            color: #2c3e50;
-            font-size: 30 /@rem;
-        }
-    }
+    border-bottom: 0.5px solid #f5f5f9;
+	.hd {
+		position: fixed;
+		top: 0;
+		left: 0;
+		right: 0;
+		bottom: 0;
+		height: 95 / @rem;
+		line-height: 95 / @rem;
+		padding: 0 75/4 / @rem;
+		background-color: white;
+		z-index: 996;
+		font-size: 32 / @rem;
+		i {
+			font-size: 32 / @rem;
+		}
+		a {
+			padding: 0 10 / @rem;
+			color: #2c3e50;
+		}
+		.search {
+			position: absolute;
+			top: 50%;
+			left: 0;
+			right: 0;
+			margin: 0 auto;
+			width: 75%;
+			height: 75%;
+			background-color: #f5f5f9;
+			transform: translateY(-50%);
+			line-height: 71 / @rem;
+			border-radius: 8px;
+			span {
+				color: #2c3e50;
+				font-size: 30 / @rem;
+			}
+		}
+	}
 }
-
 </style>
--- src/components/shops/shopList.vue
+++ src/components/shops/shopList.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="high">
         <!-- header -->
-        <header></header>
         <title-bar title="搜索商品"></title-bar>
         <!-- headerNav -->
+        <header></header>
         <div class="hdNav">
             <p>区域<i class="iconfont icon-arrow-down"></i></p>
             <p class="tbActive">综合</p>
@@ -11,15 +11,42 @@
             <p>价格</p>
             <p>筛选<i class="iconfont icon-arrow-down"></i></p>
         </div>
+        <!-- section -->
+        <section class="likeYou">
+            <ul>
+                <li v-for="(item,index) in shopLists" :key="index">
+                    <a href="javascript:;">
+                        <div class="pic"><img :src="item.imgUrl" alt="" width="100%" height="100%" v-lazy="item.imgUrl"></div>
+                        <p class="des" v-text="item.des"></p>
+                        <div class="priceInfo clearFix">
+                            <div class="fl shopPrice"><span v-text="item.symbol"></span><span v-text="item.price"></span></div>
+                            <div class="fr alreadyPaid" v-text="item.alreadyPaid"></div>
+                        </div>
+                    </a>
+                </li>
+            </ul>
+        </section>
+        <!-- footer -->
+        <footer>
+            <p>- - 暂无更多 - -</p>
+        </footer>
     </div>
 </template>
 
 <script>
 export default {
     data() {
-        return {}
+        return {
+            shopLists: []
+        }
     },
-    components: {}
+    components: {},
+    created(){
+        this.$ajax.get("http://tanzhouweb.com/vueProject/vue.php?title=likeYou")
+            .then((res)=>{
+                this.shopLists = res.data
+            })
+    }
 }
 </script>
 
@@ -28,11 +55,13 @@ export default {
 
 .high{
     padding: 0 75/4 /@rem;
+    background-color: #F7F7FA;
     header{
         height: 95 /@rem;
     }
     .hdNav{
-        position: absolute;
+        position: fixed;
+        top: 95 /@rem;
         left: 0;
         right: 0;
         margin: 0 auto;
@@ -43,12 +72,70 @@ export default {
         justify-content: space-between;
         align-items: center;
         font-size: 30 /@rem;
+        z-index: 990;
+        background-color: white;
         p{
             margin: 0 10 /@rem;
         }
         i{
             font-size: 32 /@rem;
         }
+    }
+    .likeYou{
+        position: relative;
+        overflow: hidden;
+        margin-top: 75/4 /@rem;
+        ul{
+            display: flex;
+            flex-direction: row;
+            justify-content: space-between;
+            flex-wrap: wrap;
+        }
+        ul li{
+            width: 348 /@rem;
+            height: 440 /@rem;
+            background-color: white;
+            margin-bottom: 75/4 /@rem;
+            border-radius: 5px;
+        }
+        ul li a{
+            width: 100%;
+            height: 100%;
+            display: block;
+        }
+        ul li .pic{
+            width: 100%;
+            height: 300 /@rem;
+            img{
+                border-radius: 5px 5px 2px 2px;
+            }
+        }
+        ul li .des{
+            font-size: 26 /@rem;
+            text-overflow: ellipsis;
+            white-space: nowrap;
+            overflow: hidden;
+            color: #000;
+            margin: 25/@rem 15/@rem;
+        }
+        ul li .priceInfo{
+            font-size: 26 /@rem;
+            margin: 0 20/@rem;
+            height: 38/@rem;
+            line-height: 38/@rem;
+            .shopPrice{
+                color: #FF5000;
+            }
+            .alreadyPaid{
+                color: #888;
+            }
+        }
+    }
+    footer{
+        padding-bottom: 75/4 /@rem;
+        font-size: 26 /@rem;
+        color: #888;
+        text-align: center;
     }
 }
 
