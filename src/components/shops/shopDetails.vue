8941e87151a9d9b12943355bff80ddf8bd961d26
Hrealm <952172047@qq.com>
2019-07-09 23:54:48 +0800

    更新商品详细内容

--- src/components/shops/shopDetails.vue
+++ src/components/shops/shopDetails.vue
@@ -1,7 +1,7 @@
 <template>
 	<div class="high">
 		<!-- header -->
-        <header></header>
+		<header></header>
 		<header class="hd clearFix">
 			<a href="javascript:;" class="fl" @click="goBack">
 				<i class="iconfont icon-fanhui"></i>
@@ -13,23 +13,47 @@
 				<span>商品详情</span>
 			</div>
 		</header>
-        <!-- content -->
-        <div class="shopDetails">
-            <div class="shopInfo">
-                <div class="shopPic">
-                    <img :src="shopDetails.imgUrl || 'https://static.oschina.net/uploads/space/2012/1111/054329_vbJd_63477.gif'" height="100%" width="100%" alt="">
-                </div>
-                <div class="detailInfo">
-                    <p class="money">
-                        <span class="symbol" v-text="shopDetails.symbol || '¥'"></span><span class="price" v-text="shopDetails.price || 'NaN'"></span>
-                    </p>
-                    <p class="desc" v-text="shopDetails.des || '请求数据失败'"></p>
-                    <div class="courier clearFix"><span class="fl">快递：免运费</span><span class="fr" v-text="shopDetails.alreadyPaid || 'NaN人付款'"></span></div>
-                </div>
-            </div>
-            <div class="discount"></div>
+		<!-- content -->
+		<div class="shopDetails">
+			<div class="shopInfo">
+				<div class="shopPic">
+					<img
+						:src="shopDetails.imgUrl || 'https://static.oschina.net/uploads/space/2012/1111/054329_vbJd_63477.gif'"
+						height="100%"
+						width="100%"
+						alt
+					/>
+				</div>
+				<div class="detailInfo">
+					<p class="money">
+						<span class="symbol" v-text="shopDetails.symbol || '¥'"></span>
+						<span class="price" v-text="shopDetails.price || 'NaN'"></span>
+					</p>
+					<p class="desc" v-text="shopDetails.des || '请求数据失败'"></p>
+					<div class="courier clearFix">
+						<span class="fl">快递：免运费</span>
+						<span class="fr" v-text="shopDetails.alreadyPaid || 'NaN人付款'"></span>
+					</div>
+				</div>
+			</div>
+			<div class="discount">
+				<ul>
+					<li class="clearFix">
+						<span class="fl">优惠</span>
+						<p class="fr"><span>领劵</span><i class="iconfont icon-iconfontyoujiantou"></i></p>
+						<p class="discountInfo">领劵后至少可减<span class="discountPrice">¥ 5</span></p>
+					</li>
+					<li class="clearFix">
+						<span class="fl">服务</span>
+						<p class="fr"><i class="iconfont icon-iconfontyoujiantou"></i></p>
+						<p class="discountInfo">48小时内发货 · 无忧退货</p>
+					</li>
+				</ul>
+			</div>
+			<div class="select"><p class="clearFix"><span class="fl">选择</span><i class="fr iconfont icon-iconfontyoujiantou"></i>
+                <span class="selected">请选择分类</span></p></div>
             <div class="buy"></div>
-        </div>
+		</div>
 	</div>
 </template>
 
@@ -37,21 +61,20 @@
 export default {
 	data() {
 		return {
-            shopDetails:{}
-        }
-    },
-    created(){
-        let index = this.$route.query.id;
-        if(index){
-            let title = this.$route.query.title;
-            let url = 'http://tanzhouweb.com/vueProject/vue.php' + '?title=' + title + index;
-            this.$ajax.get(url)
-                .then((res)=>{
-                    this.shopDetails = res.data;
-                    console.log(res.data);
-                })
-        }
-    },
+			shopDetails: {}
+		}
+	},
+	created() {
+		let index = this.$route.query.id
+		if (index) {
+			let title = this.$route.query.title
+			let url ='http://tanzhouweb.com/vueProject/vue.php' + '?title=' + title + index
+			this.$ajax.get(url).then(res => {
+				this.shopDetails = res.data
+				// console.log(res.data);
+			})
+		}
+	},
 	methods: {
 		goBack() {
 			this.$router.go(-1)
@@ -65,72 +88,117 @@ export default {
 @rem: 750/10rem;
 
 .high {
-    // padding: 0 75/4 /@rem;
-    background-color: #F7F7FA;
-    header{
-        height: 95 /@rem;
-    }
+	// padding: 0 75/4 /@rem;
+	background-color: #f7f7fa;
+	header {
+		height: 95 / @rem;
+	}
 	.hd {
-        position: fixed;
-        top: 0;
-        left: 0;
-        right: 0;
-        height: 95 /@rem;
-        line-height: 95 /@rem;
-        padding: 0 75/4 /@rem;
-        background-color: white;
-        border-bottom: 0.5px solid #f5f5f9;
-        font-size: 32 /@rem;
-        z-index: 999;
-        i {
-            font-size: 32 /@rem;
-        }
-        a {
+		position: fixed;
+		top: 0;
+		left: 0;
+		right: 0;
+		height: 95 / @rem;
+		line-height: 95 / @rem;
+		padding: 0 75/4 / @rem;
+		background-color: white;
+		border-bottom: 0.5px solid #f5f5f9;
+		font-size: 32 / @rem;
+		z-index: 999;
+		i {
+			font-size: 32 / @rem;
+		}
+		a {
 			padding: 0 10 / @rem;
 			color: #2c3e50;
 		}
-    }
-    .shopDetails{
-        .shopInfo{
-            background-color: white;
-            padding-bottom: 20 /@rem;
-            .shopPic{
-                width: 100%;
-                height: 650 /@rem;
-            }
-            .detailInfo{
-                padding: 0 75/4 /@rem;
-                padding-top: 25 /@rem;
-                border-top: 1px solid #f5f5f9;
-                .money{
+	}
+	.shopDetails {
+		.shopInfo {
+			background-color: white;
+			padding-bottom: 20 / @rem;
+			.shopPic {
+				width: 100%;
+				height: 650 / @rem;
+			}
+			.detailInfo {
+				padding: 0 75/4 / @rem;
+				padding-top: 25 / @rem;
+				border-top: 1px solid #f5f5f9;
+				.money {
+					text-align: start;
+					color: #ff5000;
+					font-size: 32 / @rem;
+					.price {
+						font-size: 44 / @rem;
+						margin: 0 10 / @rem;
+						font-weight: 400;
+					}
+				}
+				.desc {
+					margin-top: 8 / @rem;
+					text-align: start;
+					font-size: 30 / @rem;
+					color: #333;
+					font-weight: bold;
+				}
+				.courier {
+					margin-top: 20 / @rem;
+					font-size: 26 / @rem;
+					color: #999;
+				}
+			}
+		}
+		.discount {
+			padding: 0 75/4 / @rem;
+			margin-top: 25 / @rem;
+			// height: 200 /@rem;
+			background-color: white;
+			font-size: 28 / @rem;
+			ul {
+                padding-top: 20 /@rem;
+				li {
+					padding-bottom: 20 / @rem;
                     text-align: start;
-                    color: #FF5000;
-                    font-size: 32 /@rem;
-                    .price{
-                        font-size: 44 /@rem;
-                        margin: 0 10 /@rem;
-                        font-weight: 400;
-                    }
-                }
-                .desc{
-                    margin-top: 8 /@rem;
-                    text-align: start;
-                    font-size: 30 /@rem;
-                    color: #333;
-                    font-weight: bold;
-                }
-                .courier{
-                    margin-top: 20 /@rem;
-                    font-size: 26 /@rem;
-                    color: #999;
-                }
-            }
+                    line-height: 39 /@rem;
+					span {
+						color: #999;
+					}
+					i {
+						color: #999;
+						font-size: 26 / @rem;
+                        margin-left: 15 / @rem;
+					}
+					.discountInfo {
+                        color: #333;
+						margin-left: 100 / @rem;
+						.discountPrice {
+							margin-left: 10 / @rem;
+							color: #ff5000;
+						}
+					}
+				}
+			}
         }
-        .discount{
+        .select{
             margin-top: 25 /@rem;
-            height: 200 /@rem;
             background-color: white;
+            padding: 20 /@rem 75/4 /@rem;
+            font-size: 28 /@rem;
+            color: #999;
+            text-align: start;
+            p{
+                line-height: 39 /@rem;
+                i{
+                    font-size: 26 / @rem;
+                }
+                .selected{
+                    margin-left: 45 / @rem;
+                    color: #333;
+                }
+            }
+            
         }
-    }
+	}
 }
 </style>
